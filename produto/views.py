c9d4731836d5e182800071388b6735cc3087e6b9
roberto <rc.lima.jr@gmail.com>
2021-08-23 21:47:37 -0300

    ok por enquanto

--- produto/views.py
+++ produto/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 from django.contrib import messages
 from . import models
 
+from pprint import pprint
+
 # Create your views here.
 
 
@@ -26,6 +28,11 @@ class DetalheProduto(DetailView):
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
 
+        # TODO: APAGA O CARRINHO
+        # if self.request.session.get('carrinho'):
+        #     del self.request.session['carrinho']
+        #     self.request.session.save()
+
         http_referer = self.request.META.get(
             'HTTP_REFERER', reverse('produto:lista'))
         variacao_id = self.request.GET.get('vid')
@@ -35,21 +42,71 @@ class AdicionarAoCarrinho(View):
             return redirect(http_referer)
 
         variacao = get_object_or_404(models.Variacao, id=variacao_id)
+        variacao_estoque = variacao.estoque
+        produto = variacao.produto
 
-        if not self.request.seesion.get('carrinho'):
+        produto_id = produto.id
+        produto_nome = produto.nome
+        variacao_nome = variacao.nome or ''
+        # variacao_id = variacao.id
+        preco_unitario = variacao.preco
+        preco_unitario_promocional = variacao.preco_promocional
+        quantidade = 1
+        slug = produto.slug
+        imagem = produto.imagem
+
+        if imagem:
+            imagem = imagem.name
+        else:
+            imagem = ''
+
+        if variacao.estoque < 1:
+            messages.error(self.request, 'Estoque insuficiente')
+            return redirect(http_referer)
+
+        if not self.request.session.get('carrinho'):
             self.request.session['carrinho'] = {}
             self.request.session.save()
 
         carrinho = self.request.session['carrinho']
 
         if variacao_id in carrinho:
-            # TODO: Variação existe no  carrinho
-            pass
-        else:
-            # TODO: Variação não existe no carrinho
-            pass
+            quantidade_carrinho = carrinho[variacao_id]['quantidade']
+            quantidade_carrinho += 1
 
-        return HttpResponse(f'{variacao.produto} {variacao.nome}')
+            if variacao_estoque < quantidade_carrinho:
+                messages.warning(
+                    self.request, f'estoque insuficiente para {quantidade_carrinho}x no produto "{produto_nome}".'
+                    'Adicionamos {variacao_estoque}x no seu carrinho.')
+                quantidade_carrinho = variacao_estoque
+
+            carrinho[variacao_id]['quantidade'] = quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo'] = preco_unitario * \
+                quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo_promocional'] = preco_unitario_promocional * \
+                quantidade_carrinho
+        else:
+            carrinho[variacao_id] = {
+                'produto_id': produto_id,
+                'produto_nome': produto_nome,
+                'variacao_nome': variacao_nome,
+                'variacao_id': variacao_id,
+                'preco_unitario': preco_unitario,
+                'preco_unitario_promocional': preco_unitario_promocional,
+                'quantidade': quantidade,
+                'slug': slug,
+                'imagem': imagem,
+            }
+
+        self.request.session.save()
+        # pprint(carrinho)
+        # return HttpResponse(f'{variacao.produto} {variacao.nome}')
+
+        messages.success(
+            self.request, f'Produto {produto_nome} {variacao_nome} adicionado ao seu carrinho.'
+            f"carrinho {carrinho[variacao_id]['quantidade']}x."
+        )
+        return redirect(http_referer)
 
 
 class RemoverDoCarrinho(View):
